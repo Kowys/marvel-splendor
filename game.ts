2f68ddbff493dbe8f2d03114018810637f4c5f54
kowys <yong_sheng_kow@kelaberetiv.sg>
2024-02-10 22:59:52 +0800

    Work on card board

--- game.ts
+++ game.ts
@@ -1,7 +1,14 @@
 const btnCounter = document.querySelector("#btncounter");
+const dealCards = document.querySelector("#deal");
 let counter = 0;
 
 updateDisplay(counter);
+
+// document.querySelector("#level")!.innerHTML = `abc`;
+
+function updateDisplay(c) {
+    document.querySelector("#result")!.innerHTML = `Count: ${c}`;
+};
 
 btnCounter!.addEventListener('click', () => {
     counter += 1;
@@ -9,7 +16,8 @@ btnCounter!.addEventListener('click', () => {
     updateDisplay(counter);
 });
 
-
-function updateDisplay(c) {
-    document.querySelector("#result")!.innerHTML = `Count: ${c}`;
-};
+dealCards!.addEventListener('click', () => {
+    console.log("Card dealt!");
+    const imgURL = `<img src="./images/cards/captain-america.jpg" alt="Level 3 card" width="200", height="auto"/>`
+    document.querySelector("#level3-1")!.innerHTML = imgURL;
+});
--- ts-built/game.js
+++ ts-built/game.js
@@ -1,12 +1,19 @@
 var btnCounter = document.querySelector("#btncounter");
+var dealCards = document.querySelector("#deal");
 var counter = 0;
 updateDisplay(counter);
+// document.querySelector("#level")!.innerHTML = `abc`;
+function updateDisplay(c) {
+    document.querySelector("#result").innerHTML = "Count: ".concat(c);
+}
+;
 btnCounter.addEventListener('click', function () {
     counter += 1;
     console.log(counter);
     updateDisplay(counter);
 });
-function updateDisplay(c) {
-    document.querySelector("#result").innerHTML = "Count: ".concat(c);
-}
-;
+dealCards.addEventListener('click', function () {
+    console.log("Card dealt!");
+    var imgURL = "<img src=\"./images/cards/captain-america.jpg\" alt=\"Level 3 card\" width=\"200\", height=\"auto\"/>";
+    document.querySelector("#level3-1").innerHTML = imgURL;
+});
