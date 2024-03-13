17508dab15bec39b139959d0cde5acaff659a24d
kowys <yong_sheng_kow@kelaberetiv.sg>
2024-03-14 00:13:27 +0800

    Switch to mysql

---
+++ mysql_queries.js
@@ -0,0 +1,64 @@
+var mysql = require('mysql2');
+// SQL stuff needs to live on the server, not the browser
+// All SQL commands are to be kept on server itself
+// If possible, logic should be executed on server, not browser
+
+function createConn() {
+    return mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "kyssyk14"
+    });
+}
+
+function assertDBAndTables(conn) {
+    conn.connect(function(err) {
+        if (err) throw err;
+        console.log("Connected!");
+
+        const assertDBQuery = 'CREATE DATABASE IF NOT EXISTS kowys_test'
+        conn.query(assertDBQuery, function (error, results, fields) {
+            if (error) throw error;
+            console.log("Database created");
+        });
+
+        conn.changeUser({database: "kowys_test"});
+        var assertTableQuery = "CREATE TABLE IF NOT EXISTS counter (id INTEGER(255) PRIMARY KEY, count INTEGER(255));";
+        conn.query(assertTableQuery, function (err, result) {
+            if (err) throw err;
+            console.log("Table created");
+        });
+
+        var insertTableQuery = "INSERT IGNORE INTO counter (id, count) VALUES(1, 0);";
+        conn.query(insertTableQuery, function (err, result) {
+            if (err) throw err;
+            console.log("Row inserted");
+        });
+    });
+}
+
+function getCurrentPermCounter(conn) {
+    return new Promise((resolve, reject) => {
+        const getPermCounterQuery = 'SELECT count FROM counter WHERE id = 1'
+        conn.query(getPermCounterQuery, function (error, results, fields) {
+            if (error) throw error;
+            console.log(`Perm count: ${results[0].count}`);
+            return resolve(results[0].count);
+        });
+    });
+}
+
+function incrementPermCounter(conn, i) {
+    console.log(`Update amount: ${i}`)
+    const incrementPermCounterQuery = `UPDATE counter SET count = count + ${i} WHERE id = 1`
+    conn.query(incrementPermCounterQuery, function (error, results, fields) {
+        if (error) throw error;
+    });
+}
+
+module.exports = {
+    createConn,
+    assertDBAndTables,
+    getCurrentPermCounter,
+    incrementPermCounter
+}
--- routes/game.js
+++ routes/game.js
@@ -1,17 +1,18 @@
 var express = require('express');
-var sql = require('../sql_queries');
+var sql = require('../mysql_queries');
 
 var router = express.Router();
+var conn = sql.createConn();
 
 /* GET game state. */
 router.get('/', function(request, response, next) {
-	sql.assertDBAndTables();
+	sql.assertDBAndTables(conn);
 	response.render('interface', { title: 'Marvel Splendor', layout: "interface_layout"});
 });
 
 /* GET count from DB. */
 router.get('/get-counter', async function(request, response, next) {
-	var permCount = await sql.getCurrentPermCounter();
+	var permCount = await sql.getCurrentPermCounter(conn);
 	var params = {};
 	params.count = permCount;
 
@@ -20,7 +21,7 @@ router.get('/get-counter', async function(request, response, next) {
 
 /* POST to DB. */
 router.post('/increment-counter', function(request, response, next) {
-	sql.incrementPermCounter(request.body.increment)
+	sql.incrementPermCounter(conn, request.body.increment)
 	response.json({success: true});
 });
 
